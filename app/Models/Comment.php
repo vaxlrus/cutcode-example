d46057a175365fccf7f1e12998a9cd887b773665
vaxlrus <vaxl.rus@gmail.com>
2022-04-21 23:43:20 +0300

    fix comment model methods

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -17,11 +17,11 @@ class Comment extends Model
 
     public function post()
     {
-        $this->belongsTo(Post::class);
+        return $this->belongsTo(Post::class);
     }
 
     public function user()
     {
-        $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);
     }
 }
